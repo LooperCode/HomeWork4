a1435963d6340e176073ba57a9dfd5ab73372e1c
Denis <Savchenkodkz@gmail.com>
2022-12-06 03:25:58 +0600

    Задача 27

--- Program.cs
+++ Program.cs
@@ -1,21 +1,10 @@
-﻿Console.WriteLine("Введите число");
+﻿
+Console.WriteLine("Введите число");
 int insert = Convert.ToInt32(Console.ReadLine());
-int temp = insert;
-if (insert < 1000) {
-    temp = insert % 100;
-    temp = (insert / 100 )+ (temp /10)+ (insert%10);
+int sum = 0;
+
+while (insert > 0) {
+    sum = sum + insert % 10;
+    insert = insert / 10;
 }
-else if (insert < 10000) {
-    temp = insert % 1000/10; 
-    temp = (insert / 1000) + (temp / 10) + (temp % 10) + (insert % 10);
-}
-else if (insert < 100000){
-    temp = insert / 100 % 100;
-    temp = (insert / 10000) + (temp / 10 ) + (temp % 10) + (insert % 100 / 10) + (insert % 10)  ;
-}
-else if (insert < 1000000){
-    temp = insert / 1000;
-    temp = (temp / 100) + (temp % 100) / 10 + (temp % 10);
-    temp = temp + (insert % 1000)/100 + (insert % 1000) % 100 / 10 + (insert % 10);
-}
-Console.WriteLine(temp);
+Console.WriteLine(sum);
