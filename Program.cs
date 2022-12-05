a146b0fdcf11fdd3cb485cd41dd3cea764e516cd
Denis <Savchenkodkz@gmail.com>
2022-12-06 02:57:39 +0600

    Задача 27

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
-﻿Console.WriteLine("Введите число A");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите степень B");
-int b = Convert.ToInt32(Console.ReadLine());
-int temp = a;
-for (int i = 1; i < b ; i++){
-    a = a*temp;
+﻿Console.WriteLine("Введите число");
+int insert = Convert.ToInt32(Console.ReadLine());
+int temp = insert;
+if (insert < 1000) {
+    temp = insert % 100;
+    temp = (insert / 100 )+ (temp /10)+ (insert%10);
 }
-Console.WriteLine($"Натуральная степерь числа {temp}: {a} ");
+else if (insert < 10000) {
+    temp = insert % 1000/10; 
+    temp = (insert / 1000) + (temp / 10) + (temp % 10) + (insert % 10);
+}
+else if (insert < 100000){
+    temp = insert / 100 % 100;
+    temp = (insert / 10000) + (temp / 10 ) + (temp % 10) + (insert % 100 / 10) + (insert % 10)  ;
+}
+else if (insert < 1000000){
+    temp = insert / 1000;
+    temp = (temp / 100) + (temp % 100) / 10 + (temp % 10);
+    temp = temp + (insert % 1000)/100 + (insert % 1000) % 100 / 10 + (insert % 10);
+}
+Console.WriteLine(temp);
