3d59e39b4491d0f3ce450c95300de694f68bd94f
Denis <Savchenkodkz@gmail.com>
2022-12-05 21:55:54 +0600

    Задача 25

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень B");
+int b = Convert.ToInt32(Console.ReadLine());
+int temp = a;
+for (int i = 1; i < b ; i++){
+    a = a*temp;
+}
+Console.WriteLine($"Натуральная степерь числа {temp}: {a} ");
