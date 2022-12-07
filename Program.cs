ccec7f4ec4f4b23a73dd4ad3a252fc73d4eaf964
Denis <Savchenkodkz@gmail.com>
2022-12-08 05:53:09 +0600

    доп.задача

--- Program.cs
+++ Program.cs
@@ -1,9 +1,40 @@
-﻿Console.WriteLine("Введите число A");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите степень B");
-int b = Convert.ToInt32(Console.ReadLine());
-int temp = a;
-for (int i = 1; i < b ; i++){
-    a = a*temp;
+﻿Console.WriteLine("Введите размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new Int32[n];
+for(int i = 0; i < n; i++){
+    Console.WriteLine($"Введите значение элемента {i}");
+    
+    array[i] = Convert.ToInt32(Console.ReadLine());
 }
-Console.WriteLine($"Натуральная степерь числа {temp}: {a} ");
+Console.WriteLine("Введите количество ступеней сдвига");
+int k = Convert.ToInt32(Console.ReadLine());
+//int[] array = {5, 3, 7, 4, 6}; 
+int temp = array[0];
+//int n = 5;
+//int k = 3;
+if (k > 0){
+    for (int j = 0; j < k-1; j++){
+        for (int i = 0; i < n-1; i++){
+            temp = array[i];
+            array[i] = array[(i+1)];
+            array[i+1] = temp;
+       }
+   }
+}
+else if (k < 0) {
+    for (int j = n-1; j > k; j--){
+    for (int i = n-1; i > 0; i--){
+        temp = array[i];
+        array[i] = array[i-1];
+        array[i-1] = temp;
+    }
+}
+}
+   for (int i = 0; i < n; i++){
+    if(i == n-1){
+    Console.Write($"[{array[i]}]");
+    }
+    else{
+    Console.Write($"[{array[i]}], ");
+   }
+   }
