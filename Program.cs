297ff023c10f6c7a3a6eeb4fb03ae657fa46ac21
Denis <Savchenkodkz@gmail.com>
2022-12-12 04:10:24 +0600

    add %n

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите размер массива");
+﻿
+Console.WriteLine("Введите размер массива");
 int n = Convert.ToInt32(Console.ReadLine());
 int [] array = new Int32[n];
 for(int i = 0; i < n; i++){
@@ -8,9 +9,10 @@ for(int i = 0; i < n; i++){
 }
 Console.WriteLine("Введите количество ступеней сдвига");
 int k = Convert.ToInt32(Console.ReadLine());
+k = k % n;
 int temp = array[0];
 if (k > 0){
-    for (int j = 0; j < k-1; j++){
+    for (int j = 0; j <= k-1; j++){
         for (int i = 0; i < n-1; i++){
             temp = array[i];
             array[i] = array[(i+1)];
@@ -19,7 +21,7 @@ if (k > 0){
    }
 }
 else if (k < 0) {
-    for (int j = n-1; j > k; j--){
+    for (int j = n-1; j >= k; j--){
     for (int i = n-1; i > 0; i--){
         temp = array[i];
         array[i] = array[i-1];
@@ -35,3 +37,4 @@ else if (k < 0) {
     Console.Write($"[{array[i]}], ");
    }
    }
+   
